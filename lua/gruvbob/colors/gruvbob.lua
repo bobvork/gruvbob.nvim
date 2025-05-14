3c6d00f26a7854c585403aa42cb3abe55a96c62a
Bob Vork <bobvork@fastmail.nl>
2025-05-14 20:43:43 +0200

    lets see how the syntax hightlighting looks now

--- lua/gruvbob/colors/gruvbob.lua
+++ lua/gruvbob/colors/gruvbob.lua
@@ -7,76 +7,6 @@ local palette = {
 
     none = 'NONE',
 
-    -- Blacks. Not in base Nord.
-    black0 = '#191D24',
-    black1 = '#1e2122',
-    -- Slightly darker than bg.  Very useful for certain popups
-    black2 = '#1a1d1e',
-
-    -- Grays
-    -- This color is used on their website's dark theme.
-    gray0 = '#484b4c', --bg
-    -- Polar Night.
-    gray1 = '#2E3440',
-    gray2 = '#575a5b',
-    gray3 = '#545758',
-    gray4 = '#606364',
-
-    -- A light blue/gray.
-    gray5 = '#606364',
-
-    -- Dim white.
-    -- default fg, has a blue tint.
-    white0_normal = '#c7b89d',
-    -- less blue tint
-    white0_reduce_blue = '#c7b89d',
-
-    -- Snow storm.
-    white1 = '#c7b89d',
-    white2 = '#c7b89d',
-    white3 = '#c7b89d',
-
-    -- Frost.
-    blue0 = '#5E81AC',
-    blue1 = '#81A1C1',
-    blue2 = '#88C0D0',
-
-    cyan = {
-        base = '#8FBCBB',
-        bright = '#9FC6C5',
-        dim = '#80B3B2',
-    },
-
-    -- Aurora.
-    -- These colors are used a lot, so we need variations for them.
-    -- Base colors are from original Nord palette.
-    red = {
-        base = '#ec6b64',
-        bright = '#ec6b64',
-        dim = '#ec6b64',
-    },
-    orange = {
-        base = '#e78a4e',
-        bright = '#e78a4e',
-        dim = '#e78a4e',
-    },
-    yellow = {
-        base = '#d6b676',
-        bright = '#d6b676',
-        dim = '#d6b676',
-    },
-    green = {
-        base = '#89b482',
-        bright = '#a9b665',
-        dim = '#89b482',
-    },
-    magenta = {
-        base = '#9385b4',
-        bright = '#9385b4',
-        dim = '#887aa9',
-    },
-
-
     ---- copied over from nvchad theme for reference
     base00 = "#1e2122",
     base01 = "#2c2f30",
@@ -95,38 +25,40 @@ local palette = {
     base0E = "#d3869b",
     base0F = "#d65d0e",
 
-    chad = {
-        white = "#c7b89d",
-        darker_black = "#1a1d1e",
-        black = "#1e2122", --  nvim bg
-        black2 = "#242728",
-        one_bg = "#282b2c",
-        one_bg2 = "#393c3d",
-        one_bg3 = "#404344",
-        grey = "#484b4c",
-        grey_fg = "#575a5b",
-        grey_fg2 = "#545758",
-        light_grey = "#606364",
-        red = "#ec6b64",
-        baby_pink = "#ce8196",
-        pink = "#ff75a0",
-        line = "#323536", -- for lines like vertsplit
-        green = "#89b482",
-        vibrant_green = "#a9b665",
-        nord_blue = "#6f8faf",
-        blue = "#6d8dad",
-        yellow = "#d6b676",
-        sun = "#d1b171",
-        purple = "#9385b4",
-        dark_purple = "#887aa9",
-        teal = "#749689",
-        orange = "#e78a4e",
-        cyan = "#82b3a8",
-        statusline_bg = "#222526",
-        lightbg = "#2d3031",
-        pmenu_bg = "#89b482",
-        folder_bg = "#6d8dad",
-    }
+    white = "#c7b89d",
+    white_bright = "#d6c8af",
+    darker_black = "#1a1d1e",
+    black = "#1e2122", --  nvim bg
+    black2 = "#242728",
+    one_bg = "#282b2c",
+    one_bg2 = "#393c3d",
+    one_bg3 = "#404344",
+    grey = "#484b4c",
+    grey_fg = "#575a5b",
+    grey_fg2 = "#545758",
+    light_grey = "#606364",
+    red = "#ec6b64",
+    baby_pink = "#ce8196",
+    pink = "#ff75a0",
+    line = "#323536", -- for lines like vertsplit
+    green = "#89b482",
+    vibrant_green = "#a9b665",
+    nord_blue = "#6f8faf",
+    blue = "#6d8dad",
+    yellow = "#d6b676",
+    sun = "#d1b171",
+    purple = "#9385b4",
+    dark_purple = "#887aa9",
+    teal = "#749689",
+    orange = "#e78a4e",
+    cyan = "#82b3a8",
+    statusline_bg = "#222526",
+    lightbg = "#2d3031",
+    pmenu_bg = "#89b482",
+    folder_bg = "#6d8dad",
+
+    -- For debugging
+    foo = "#ff0000"
 }
 
 return palette
--- lua/gruvbob/colors/init.lua
+++ lua/gruvbob/colors/init.lua
@@ -10,62 +10,52 @@ function C.build_palette()
 
     local options = require('gruvbob.config').options
 
-    -- `white0` is used as the default fg, and has a blue tint.
-    -- Reduce that amount of tint.
-    C.white0 = (options.reduced_blue and C.white0_reduce_blue) or C.white0_normal
-
     -- Modify the palette before generating colors.
     options.on_palette(C)
-
-    -- Add these for international convenience :)
-    C.grey0 = C.gray0
-    C.grey1 = C.gray1
-    C.grey2 = C.gray2
-    C.grey3 = C.gray3
-    C.grey4 = C.gray4
-    C.grey5 = C.gray5
 
     -- Define some use cases.
     -- Some of the format is from @folke/tokyonight.nvim.
 
+    C.white0 = C.white
+
     -- Backgrounds
-    C.bg = (options.transparent.bg and C.none) or ((options.swap_backgrounds and C.black1) or C.gray0)
-    C.bg_dark = (options.transparent.bg and C.none) or C.black0
+    C.bg = (options.transparent.bg and C.none) or ((options.swap_backgrounds and C.black) or C.black2)
+    C.bg_dark = (options.transparent.bg and C.none) or C.black
     C.bg_sidebar = (options.transparent.bg and C.none) or C.bg
-    C.bg_statusline = C.black0
-    C.bg_fold = C.gray2
+    C.bg_statusline = C.black
+    C.bg_fold = C.grey2
 
     -- Cursorline Background
     if options.cursorline.theme == 'light' then
-        options.cursorline.bg = C.gray2
+        options.cursorline.bg = C.one_bg
     else
-        options.cursorline.bg = C.black0
+        options.cursorline.bg = C.darker_black
     end
 
     C.bg_visual = (options.transparent.bg and options.cursorline.bg)
         or U.blend(options.cursorline.bg, C.bg, options.cursorline.blend)
 
     -- Borders
-    C.border_fg = (options.bright_border and C.white0) or C.black0
+    C.border_fg = (options.bright_border and C.white0) or C.darker_black
     C.border_bg = (options.transparent.bg and C.none) or C.bg
 
     -- Foregrounds
     C.fg = C.white0
-    C.fg_bright = C.white1
+    C.fg_bright = C.white_bright
     C.fg_dark = C.white0
-    C.fg_sidebar = C.gray2
+    C.fg_sidebar = C.grey_fg
     C.fg_fold = C.fg
     C.fg_selected = C.fg_bright
 
     -- Floating windows
-    C.bg_float = (options.transparent.float and C.none) or ((options.swap_backgrounds and C.gray0) or C.black1)
+    C.bg_float = (options.transparent.float and C.none) or ((options.swap_backgrounds and C.black2) or C.black)
     C.fg_float = C.fg
     C.bg_float_border = C.bg_float
     C.fg_float_border = C.border_fg
 
     -- Popups
     C.bg_popup = C.bg_float
-    C.bg_selected = C.gray2
+    C.bg_selected = C.one
     C.fg_popup = C.fg
     C.bg_popup_border = C.bg
     C.fg_popup_border = C.border_fg
@@ -73,28 +63,28 @@ function C.build_palette()
     -- Diffs
     local diff_blend = 0.2
     C.diff = {
-        change0 = U.blend(C.blue1, C.bg, 0.05),
-        change1 = U.blend(C.blue2, C.bg, diff_blend),
-        add = U.blend(C.green.base, C.bg, diff_blend),
-        delete = U.blend(C.red.base, C.bg, diff_blend),
+        change0 = U.blend(C.grey_fg, C.bg, 0.05),
+        change1 = U.blend(C.grey_fg, C.bg, diff_blend),
+        add = U.blend(C.green, C.bg, diff_blend),
+        delete = U.blend(C.red, C.bg, diff_blend),
     }
 
     -- Git
     C.git = {
-        add = C.green.base,
-        delete = C.red.base,
-        change = C.blue1,
+        add = C.green,
+        delete = C.red,
+        change = C.grey_fg,
     }
 
     -- Diagnostics
-    C.error = C.red.bright
-    C.warn = C.yellow.base
+    C.error = C.red
+    C.warn = C.yellow
     C.warning = C.warn
-    C.hint = C.green.bright
-    C.info = C.blue2
+    C.hint = C.green
+    C.info = C.nord_blue
 
     -- Misc
-    C.comment = C.gray4
+    C.comment = C.grey
 
     -- Modify the palette after generating colors.
     options.after_palette(C)
